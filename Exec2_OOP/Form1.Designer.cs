e258b99385c3c4c0ba3ea1affc0f0dd820a21f39
vintagehsieh <vintagehsieh@gmail.com>
2022-11-01 23:27:56 +0800

    加入專案檔案。

---
+++ Exec2_OOP/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Exec2_OOP4
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tryTextBox = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.inputTextBox = new System.Windows.Forms.TextBox();
+			this.newGameButton = new System.Windows.Forms.Button();
+			this.tryButton = new System.Windows.Forms.Button();
+			this.answerLabel = new System.Windows.Forms.Label();
+			this.titleLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// tryTextBox
+			// 
+			this.tryTextBox.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.tryTextBox.Location = new System.Drawing.Point(110, 219);
+			this.tryTextBox.Multiline = true;
+			this.tryTextBox.Name = "tryTextBox";
+			this.tryTextBox.ReadOnly = true;
+			this.tryTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.tryTextBox.Size = new System.Drawing.Size(324, 314);
+			this.tryTextBox.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("微軟正黑體", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label1.Location = new System.Drawing.Point(105, 116);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(166, 28);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "請輸入四位數字";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// inputTextBox
+			// 
+			this.inputTextBox.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.inputTextBox.Location = new System.Drawing.Point(110, 156);
+			this.inputTextBox.Name = "inputTextBox";
+			this.inputTextBox.Size = new System.Drawing.Size(154, 39);
+			this.inputTextBox.TabIndex = 2;
+			this.inputTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// newGameButton
+			// 
+			this.newGameButton.Font = new System.Drawing.Font("微軟正黑體", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.newGameButton.Location = new System.Drawing.Point(328, 111);
+			this.newGameButton.Name = "newGameButton";
+			this.newGameButton.Size = new System.Drawing.Size(106, 39);
+			this.newGameButton.TabIndex = 3;
+			this.newGameButton.Text = "新遊戲";
+			this.newGameButton.UseVisualStyleBackColor = true;
+			this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+			// 
+			// tryButton
+			// 
+			this.tryButton.Font = new System.Drawing.Font("微軟正黑體", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.tryButton.Location = new System.Drawing.Point(328, 156);
+			this.tryButton.Name = "tryButton";
+			this.tryButton.Size = new System.Drawing.Size(106, 39);
+			this.tryButton.TabIndex = 4;
+			this.tryButton.Text = "試試看";
+			this.tryButton.UseVisualStyleBackColor = true;
+			this.tryButton.Click += new System.EventHandler(this.tryButton_Click);
+			// 
+			// answerLabel
+			// 
+			this.answerLabel.AutoSize = true;
+			this.answerLabel.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.answerLabel.Location = new System.Drawing.Point(106, 571);
+			this.answerLabel.Name = "answerLabel";
+			this.answerLabel.Size = new System.Drawing.Size(62, 31);
+			this.answerLabel.TabIndex = 5;
+			this.answerLabel.Text = "答案";
+			// 
+			// titleLabel
+			// 
+			this.titleLabel.AutoSize = true;
+			this.titleLabel.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.titleLabel.Location = new System.Drawing.Point(158, 32);
+			this.titleLabel.Name = "titleLabel";
+			this.titleLabel.Size = new System.Drawing.Size(228, 46);
+			this.titleLabel.TabIndex = 6;
+			this.titleLabel.Text = "1A2B 猜遊戲";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(566, 660);
+			this.Controls.Add(this.titleLabel);
+			this.Controls.Add(this.answerLabel);
+			this.Controls.Add(this.tryButton);
+			this.Controls.Add(this.newGameButton);
+			this.Controls.Add(this.inputTextBox);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.tryTextBox);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox tryTextBox;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox inputTextBox;
+		private System.Windows.Forms.Button newGameButton;
+		private System.Windows.Forms.Button tryButton;
+		private System.Windows.Forms.Label answerLabel;
+		private System.Windows.Forms.Label titleLabel;
+	}
+}
+
---
+++ Exec2_OOP/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace Exec2_OOP4
+{
+	public partial class Form1 : Form
+	{
+		private GuessNumber game;
+		public Form1()
+		{
+			InitializeComponent();
+			game = new GuessNumber();
+			game.newGame();
+			answerLabel.Text = string.Empty;
+		}
+
+		private void newGameButton_Click(object sender, EventArgs e)
+		{
+			game = new GuessNumber();
+			game.newGame();
+
+			/*偷看答案
+			int result = 0;
+			int multi = 1;
+
+			for (int i = 3; i >= 0 ; i--)
+			{
+				result += game.answer[i] * multi;
+				multi *= 10;
+			}
+
+			answerLabel.Text = result.ToString("D4");
+			*/
+		}
+		
+		private void tryButton_Click(object sender, EventArgs e)
+		{
+			string input = inputTextBox.Text;
+			int guessNumber = 0;
+
+			try
+			{
+				guessNumber = GetInputNumber(input);
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+
+			var result = game.Guess(guessNumber.ToString("D4"));
+
+			if (result.isSucess == true)
+			{
+				MessageBox.Show("您答對了！");
+				tryTextBox.Text = $"{guessNumber:D4} -> {result.Hint}\r\n";
+				answerLabel.Text = $"答案是{guessNumber:D4}，\r\n您總共猜了{game.count}次。";
+
+			}
+			else
+			{
+				tryTextBox.Text += $"{guessNumber:D4} -> {result.Hint}\r\n";
+			}
+		}
+		
+		public int GetInputNumber(string input)
+		{
+			input = this.inputTextBox.Text;
+			return Check(input);
+		}
+
+		public int Check(string input)
+		{
+			if (string.IsNullOrEmpty(input)) throw new Exception($"您沒有填入任何數字");
+			bool isInt = int.TryParse(input, out int number);
+			return (input.Length != 4) ? throw new Exception($"數字長度須為四") : isInt ? number : throw new Exception($"僅可填入數字");
+		}
+		
+	}
+
+	public class GuessNumber
+	{
+		public int[] answer;
+		public int a = 0;
+		public int b = 0;
+		public int count = 0;
+
+		public void newGame()
+		{
+			//生成陣列
+			int[] answerNumber = new int[4];
+
+			//產生隨機數
+			Random random = new Random(Guid.NewGuid().GetHashCode());
+
+			//驗證
+			for (int i = 0; i < answerNumber.Length; i++)
+			{
+				answerNumber[i] = random.Next(0, 10); //可以0開頭
+				
+				for (int k = 0; k < i; k++)
+				{
+					while (answerNumber[i] == answerNumber[k])
+					{
+						k = 0;
+						answerNumber[i] = random.Next(0, 10);
+					}
+				}
+
+			}
+
+			this.answer = answerNumber;
+		}
+
+		public GuessResult Guess(string input)
+		{
+			a = 0;
+			b = 0;
+			count++;
+
+			int[] inputarray = new int[4];
+
+			
+			for (int i = 0; i < input.Length; i++)
+			{
+				inputarray[i] = int.Parse(input[i].ToString());
+			}
+			
+
+			for (int i = 0; i < inputarray.Length; i++)
+			{
+				for (int j = 0; j < inputarray.Length; j++)
+				{
+					if (inputarray[i] == this.answer[j])
+					{
+						if (i == j)
+						{
+							a++;
+							break;
+						}
+						else
+						{
+							b++;
+							break;
+						}
+					}
+				}
+			}
+
+			if (a == 4)
+			{
+				return GuessResult.Sucess(Hint);
+			}
+
+			return GuessResult.Failed(Hint);
+		}
+
+		public string Hint
+		{
+			get => $"{a}A{b}B";
+		}
+	}
+
+	public class GuessResult
+	{
+		public static GuessResult Sucess(string message)
+		{
+			return new GuessResult { isSucess = true, Hint = message };
+		}
+		public static GuessResult Failed(string message)
+		{
+			return new GuessResult { isSucess = false, Hint = message };
+		}
+		public bool isFailed
+		{
+			get { return !isSucess; }
+		}
+
+		public bool isSucess { get; set; }
+
+		public string Hint { get; set; }
+
+	}
+}
